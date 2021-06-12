c20ca2a5c87830fac7b5866ef69860752e47eba1
Rafael Jung <jungrafael@jungsoft.io>
2021-06-12 16:48:33 -0300

    Enable setting ENDPOINT_URL

--- dobackup.sh
+++ dobackup.sh
@@ -18,7 +18,7 @@ fi
 echo "creating archive"
 tar -zcvf $FILE_NAME $TARGET
 echo "uploading archive to S3 [$FILE_NAME, storage class - $S3_STORAGE_CLASS]"
-aws s3 cp --storage-class $S3_STORAGE_CLASS $FILE_NAME $S3_BUCKET_URL
+aws --endpoint-url $ENDPOINT_URL s3 cp --storage-class $S3_STORAGE_CLASS $FILE_NAME $S3_BUCKET_URL
 echo "removing local archive"
 rm $FILE_NAME
 echo "done"
